52753e31de541f7da00c1fc3639b056470356258
Yuchen Wang <yulon@me.com>
2016-05-09 00:46:12 +0200

    Fixed load time

--- eater.js
+++ eater.js
@@ -1,16 +1,24 @@
-var rightColPeck = document.querySelector('#right_col_peck');
-var peckCdn = rightColPeck.querySelector('.peck-status .peck-cdn');
-if (peckCdn) {
-	(function() {
-		if (rightColPeck.style.display === "block") {
-			if (peckCdn.innerText === "领取") {
-				var click = document.createEvent('MouseEvents');
-				click.initEvent('click', true, true);
-				peckCdn.dispatchEvent(click);
+function main() {
+	var rightColPeck = document.querySelector('#right_col_peck');
+	var peckCdn = rightColPeck.querySelector('.peck-status .peck-cdn');
+	if (peckCdn) {
+		(function() {
+			if (rightColPeck.style.display === "block") {
+				if (peckCdn.innerText === "领取") {
+					var click = document.createEvent('MouseEvents');
+					click.initEvent('click', true, true);
+					peckCdn.dispatchEvent(click);
+				}
+				setTimeout(arguments.callee, Math.random() * 100);
+			} else {
+				setTimeout(arguments.callee, 1000);
 			}
-			setTimeout(arguments.callee, Math.random() * 100);
-		} else {
-			setTimeout(arguments.callee, 1000);
-		}
-	})();
+		})();
+	}
+}
+
+if (document.readyState === "complete") {
+	main();
+} else {
+	window.addEventListener("load", main, false);
 }
